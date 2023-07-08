bd1e570079da5bce6d20cbbbcd244138918bfe25
Do Hyung Kim <wjp229@naver.com>
2023-07-08 22:43:20 +0900

    Algorithm 230708

--- Algorithm/17608.cpp
+++ Algorithm/17608.cpp
@@ -4,9 +4,33 @@
 
 using namespace std;
 
-int main()
+int sol17608()
 {
+    int Case = 0;
+    cin >> Case;
 
+    stack<int> Bong;
+    
+    for(int i = 0; i < Case; i++)
+    {
+        int Length = 0;
+        cin >> Length;
+        
+        if(Bong.empty())
+        {
+            Bong.push(Length);
+        }
+
+        while(!Bong.empty() && Bong.top() <= Length)
+        {
+            Bong.pop();
+        }
+
+        Bong.push(Length);
+    }
+    
+    
+    cout << Bong.size();
 
     return 0;
 }
---
+++ Algorithm/18258.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int TestCase = 0;
+    cin >> TestCase;
+
+    int que[2000000];
+    int head = 0;
+    int tail = 0;
+
+    for (int i = 0; i < TestCase; i++)
+    {
+        string instruction;
+
+        cin >> instruction;
+
+        if (instruction == "push")
+        {
+            int res = 0;
+            cin >> res;
+            que[tail] = res;
+            tail++;
+        }
+        
+        if (instruction == "pop")
+        {
+            if (tail - head == 0)
+            {
+                cout << -1 << endl;
+            }
+            else
+            {
+                cout << que[head] << endl;
+                head++;
+            }
+        }
+        if (instruction == "size")
+        {
+            cout << tail - head << endl;
+        }
+        if (instruction == "empty")
+        {
+            if (tail - head == 0)
+            {
+                cout << 1 << endl;
+            }
+            else
+            {
+                cout << 0 << endl;
+            }
+        }
+        if (instruction == "front")
+        {
+            if (tail - head == 0)
+            {
+                cout << -1 << endl;
+            }
+            else
+            {
+                cout << que[head] << endl;
+            }
+        }
+        if (instruction == "back")
+        {
+            if (tail - head == 0)
+            {
+                cout << -1 << endl;
+            }
+            else
+            {
+                cout << que[tail-1] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
